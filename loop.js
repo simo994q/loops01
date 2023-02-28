70867741d021ee30b33fcfb47ab8d65e7c02c98c
simo994q <simonkn1@hotmail.com>
2023-02-28 13:38:22 +0100

    eeqewqe

--- loop.js
+++ loop.js
@@ -3,6 +3,10 @@ for (let i = 1; i < 73; i++) {
     console.log(i);
 }
 
-for (let i = 5; i < 6 && i > 0; i--) {
+// for (let i = 5; i < 6 && i > 0; i--) {
+//     alert(i + "!")
+// }
+
+for (let i = 5; i > 0; i--) {
     alert(i + "!")
 }
